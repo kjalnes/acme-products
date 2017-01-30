31e221d6c93d24679015bde8c95423873c5580d1
kjalnes <kris.alnes@gmail.com>
2017-01-30 17:34:25 -0500

    done

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ const path = require('path');
 const Products = require('./product.model');
 const bodyParser = require('body-parser');
 
-
 nunjucks.configure('views', {
     autoescape: true,
     express: app,
@@ -15,29 +14,18 @@ nunjucks.configure('views', {
 });
 
 // create a static route for any calls to a file that exist in node_modules (i.e need bootstrap.js)
-
 app.use( express.static(path.join(__dirname, 'node_modules')));
 
 // this allows us to call req.body in products.js
 app.use(bodyParser.urlencoded({ extended: false }));
-
 app.use(methodOverride("_method"));
-
 
 app.set('view engine', 'html');
 app.engine('html', nunjucks.render);
 
-
-// makes the server always access the routes in the routes folder when we get a request:
+// access routes in routes folder
 const routes = require('./routes/product.routes');
 app.use('/', routes);
-
-
-
-
-// we are gonne use methodOverride to use delete on posts with DELETE
-
-
 
 const port = process.env.PORT || 3000;
 
